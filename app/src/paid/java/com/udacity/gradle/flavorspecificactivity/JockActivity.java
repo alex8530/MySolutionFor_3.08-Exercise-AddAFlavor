7f6b30c9fa23bbda34f314dd0f3d06a7614b1aec
alex <aliabuharb8530@gmail.com>
2018-11-09 18:41:01 +0200

    init

---
+++ app/src/paid/java/com/udacity/gradle/flavorspecificactivity/JockActivity.java
@@ -0,0 +1,15 @@
+package com.udacity.gradle.flavorspecificactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jock);
+
+
+    }
+}
